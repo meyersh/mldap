34f5cdcd719b8bfc6425331085c7c6b409831470
Shaun Meyer <meyersh@morningside.edu>
2012-03-15 10:28:40 -0500

    Polished some things in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 
 from distutils.core import setup
 setup(name='mldap',
+      description=
+        'A simple interface for manipulating Active Directory using LDAP',
+      author='Shaun Meyer',
+      author_email='meyersh@morningside.edu',
+      url='http://github.com/meyersh/mldap',
       version='1.0',
       py_modules=['mldap'],
+      requires=['ldap'],
       )
