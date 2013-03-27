547ed04a96e3eb2f82316a04adbd7f71970b584f
Shaun Meyer <meyersh@morningside.edu>
2013-03-27 15:25:29 -0500

    Working to get mldap PIP compatible.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
-setup(name='mldap',
-      description=
-        'A simple interface for manipulating Active Directory using LDAP',
-      author='Shaun Meyer',
-      author_email='meyersh@morningside.edu',
-      url='http://github.com/meyersh/mldap',
-      version='1.0',
-      packages=['mldap'],
-      requires=['ldap'],
-      )
+
+setup(
+    name='mldap',
+    description=
+    'A simple interface for manipulating Active Directory using LDAP',
+    author='Shaun Meyer',
+    author_email='meyersh@morningside.edu',
+    url='http://github.com/meyersh/mldap',
+    version='2.0',
+    packages=['mldap'],
+    requires=['python-ldap'],
+    )
