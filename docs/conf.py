5132d251f58f45fdf35823c3b957744900089050
Shaun Meyer <meyersh@morningside.edu>
2014-01-29 13:36:15 -0600

    doc: Add ldap.filter to MOCK_MODULES.

--- docs/conf.py
+++ docs/conf.py
@@ -262,6 +262,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['ldap']
+MOCK_MODULES = ['ldap', 'ldap.filter']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
